7765649b9315a2724049a4ca0e216f2a7459417d
emredermann <emre.derman@ug.bilkent.edu.tr>
2021-04-10 00:42:38 +0300

    sbmem_remove and init edit

--- sbmemlib.c
+++ sbmemlib.c
@@ -6,17 +6,18 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include <sys/mman.h>
 
-//Check Those links for shared memor segment
+//Check Those links for shared memory segment
 //https://stackoverflow.com/questions/29238015/creating-shared-memory-segments
 //https://stackoverflow.com/questions/2261582/changing-existing-shared-memory-segment-size
 
- int fd;
+int fd;
  
 
 int sbmem_init(int segsize){
-    
+
     // Removes the previous (if exist) shared memory.
     if (shm_unlink( "/sharedMem" ))
     {
@@ -36,4 +37,16 @@ int sbmem_init(int segsize){
         return -1;
     }
     return 0;
+}
+
+
+// Have doubts about 
+// The created semaphore(s) will be removed as well. 
+bool sbmem_remove (){
+
+    if(shm_unlink("/sharedMem"))
+        return true;
+    
+    sem_destroy();
+    return false;
 }
