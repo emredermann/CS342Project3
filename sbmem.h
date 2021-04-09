dbf1c603fa2dc6858a4d1fefa4fc6c209052b78d
emredermann <emre.derman@ug.bilkent.edu.tr>
2021-04-10 00:06:22 +0300

    sbmem func implementation

--- sbmem.h
+++ sbmem.h
@@ -1,11 +1,21 @@
-int sbmem_init (int segsize);
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
+
+/*
+* Segsize is in bytes, must be power of 2
+* Creation and initialization of a shared memory segment of the given size.
+* Allocated in the requesting processes.
+*/
+int sbmem_init (int segsize);
 
 /*
 use shm_unlink () function to implement this. The function
 will do all the necessary cleanup.
 */
-sbmem_remove ();
+bool sbmem_remove ();
 
 
 int sbmem_open();
---
+++ sbmemlib.c
@@ -0,0 +1,39 @@
+#include "sbmem.h"
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+//Check Those links for shared memor segment
+//https://stackoverflow.com/questions/29238015/creating-shared-memory-segments
+//https://stackoverflow.com/questions/2261582/changing-existing-shared-memory-segment-size
+
+ int fd;
+ 
+
+int sbmem_init(int segsize){
+    
+    // Removes the previous (if exist) shared memory.
+    if (shm_unlink( "/sharedMem" ))
+    {
+        printf("Shared memory unlinked.");
+    }
+
+    fd = shm_open("/sharedMem",O_RDWR | O_CREAT, 0777 );
+
+  if(fd == -1){
+      printf("Error Open shared memory open \n");
+      return -1;
+  }  
+    /* Set the memory object's size  */
+    //The size of part ??
+    if( ftruncate( fd, sizeof( segsize ) ) == -1 ) {
+        printf("ftruncate error \n");
+        return -1;
+    }
+    return 0;
+}
