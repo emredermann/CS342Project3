062b04fdb8225cfc5bd449b0400d0b5a208f2544
emredermann <emre.derman@ug.bilkent.edu.tr>
2021-04-20 21:53:17 +0300

    .

--- sbmemlib.c
+++ sbmemlib.c
@@ -144,7 +144,6 @@ void createNewFreeSpace(block * headOfTheNextNode){
 int sbmem_init(int segsize){
     
     // Removes the previous (if exist) shared memory.
-  
     if (shm_unlink( "/sharedMem" ))
     {
         printf("Shared memory unlinked.");
@@ -154,7 +153,6 @@ int sbmem_init(int segsize){
     // Creates shared memory
     fd = shm_open("/sharedMem",O_RDWR | O_CREAT, 0777 );
     
-    
     if(fd == -1){
       printf("Error Open shared memory open \n");
      
@@ -162,7 +160,7 @@ int sbmem_init(int segsize){
    }  
     /* Set the memory object's size  */
     //The size of part ??
-    if( ftruncate( fd, sizeof( nextPower(segsize) ) ) == -1 ) {
+    if( ftruncate( fd, sizeof( nextPower(segsize) * 9) ) == -1 ) {
         printf("ftruncate error \n");
         return -1;
     }
