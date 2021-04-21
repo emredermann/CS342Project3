f35c1e594b5f5b4543fb23b521e41423a0528dfb
emredermann <emre.derman@ug.bilkent.edu.tr>
2021-04-22 00:58:32 +0300

    .

---
+++ newLib.c
@@ -0,0 +1,42 @@
+#include "sbmem.h"
+#include <stdio.h>
+#include <math.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+
+struct block{
+    int address;
+    int limit;
+    struct block * next;
+};
+
+int sbmem_init (int segsize){
+    int fd;
+    if (shm_unlink( "/sharedMem" ))
+    {
+        printf("Shared memory unlinked.");
+        return 0;
+    }
+    fd = shm_open("/sharedMem",O_RDWR | O_CREAT, 0777 );
+
+    if(fd == -1){
+      printf("Error Open shared memory open \n");
+     
+      return -1;
+   }  
+
+   if(segsize > 262144 || segsize < 32768){
+       printf("Error");
+       exit(-1);
+   }
+    if( ftruncate( fd,  ) == -1 ) {
+        printf("ftruncate error \n");
+        return -1;
+    }
+}
