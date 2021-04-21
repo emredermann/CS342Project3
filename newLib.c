f495e62a3812fbdf9a868bcf3288ed0a4af0bd79
emredermann <emre.derman@ug.bilkent.edu.tr>
2021-04-22 01:25:06 +0300

    .

--- newLib.c
+++ newLib.c
@@ -16,12 +16,15 @@ struct block{
     struct block * next;
 };
 
-block * p_map;
+struct block * p_map;
 bool activeProcess;
 int virtualAddress;
+int SEG_SIZE;
 
 int sbmem_init (int segsize){
     int fd;
+    virtualAddress = 8;
+    SEG_SIZE = segsize;
     if (shm_unlink( "/sharedMem" ))
     {
         printf("Shared memory unlinked.");
@@ -44,10 +47,10 @@ int sbmem_init (int segsize){
         return -1;
     }
     //Initializes the shared memory mapps it to the p_map
-    // Size of the mapped segment.
-    p_map = (block *) mmap( virtualAddress, segsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0 );
-    virtualAddress += sizeof(block) * 9;
-    //linkedlistInit(p_map);
+    p_map = (struct block *) mmap( virtualAddress, segsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0 );
+
+
+    linkedlistInit(p_map);
     
     if(p_map == MAP_FAILED){
         printf( "Mmap failed: \n");
@@ -56,3 +59,29 @@ int sbmem_init (int segsize){
     return 0;
     
 }
+
+
+void linkedlistInit(struct block * target){
+
+    int i  = 1;
+    struct block * new_block;
+    new_block->limit=pow(2,i);
+    new_block->address = 8;
+    target->next = new_block;
+    
+    
+    
+    while(pow(2,i)< SEG_SIZE){
+        struct block * tmp_block;
+        tmp_block->limit=pow(2,i);
+        tmp_block->address = new_block->address + new_block->limit;
+        i++;
+        new_block->next = tmp_block;
+        new_block = tmp_block;
+    }    
+
+}
+
+
+
+void sbmem_remove (){}
