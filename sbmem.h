64a1101de72d2efbcdb52c092bc2c90f2268ed73
emredermann <emre.derman@ug.bilkent.edu.tr>
2021-04-22 13:55:48 +0300

    .

--- sbmem.h
+++ sbmem.h
@@ -4,19 +4,10 @@
 #include <sys/mman.h>
 
 
-/*
-* Segsize is in bytes, must be power of 2
-* Creation and initialization of a shared memory segment of the given size.
-* Allocated in the requesting processes.
-*/
 int sbmem_init (int segsize);
 
-/*
-use shm_unlink () function to implement this. The function
-will do all the necessary cleanup.
-*/
 void sbmem_remove();
 int sbmem_open();
-void *sbmem_alloc (int reqsize);
+void * sbmem_alloc (int reqsize);
 void sbmem_free (void *ptr);
 int sbmem_close ();
