94b66071fb352b38d9f59a94284c0d4f145d84b3
dogatansel <71971401+dogatansel@users.noreply.github.com>
2021-04-22 20:17:58 +0300

    Update create_memory_sb.c

--- create_memory_sb.c
+++ create_memory_sb.c
@@ -6,13 +6,13 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Başlıyoruz");
+    printf("CREATE Başlıyoruz\n");
     int segment_size;
     if (argc > 1) {
         segment_size = atoi(argv[1]);  
     }
 
     sbmem_init(segment_size);
-     printf("Kapatıyoruz");
+     printf("CREATE Kapatıyoruz\n");
     return 0;
 }
